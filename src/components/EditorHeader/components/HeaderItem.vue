b62beb5a6db8d80cdfd57a81f7bdb856ecb0246e
marckraw <marckraw@icloud.com>
2023-02-19 17:09:15 +0100

    fix: stylelint fixes

--- src/components/EditorHeader/components/HeaderItem.vue
+++ src/components/EditorHeader/components/HeaderItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="display: flex">
+  <div style="display: flex;">
     <div
       class="sb-editor-header__item"
       :class="{ 'sb-editor-header__separator': withSeparator }"
